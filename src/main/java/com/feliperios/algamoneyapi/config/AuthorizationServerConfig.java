f5d4f1b3f02644939a5b9e7cbda8cb66bf2d5876
Felipe Rios <felipedeoliveirarios@gmail.com>
2021-11-01 12:05:17 -0300

    Autenticação via JWT, com RefreshToken em cookie (Módulo 6.8).

--- src/main/java/com/feliperios/algamoneyapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/feliperios/algamoneyapi/config/AuthorizationServerConfig.java
@@ -9,11 +9,15 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	private int tokenTTL = 60;
+	private int refreshTokenTTL = 3600;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -24,21 +28,45 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.withClient("angular")
 				.secret("{noop}4ngul4r")
 				.scopes("read", "write")
-				.authorizedGrantTypes("password")
-				.accessTokenValiditySeconds(1800);
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(tokenTTL)
+				.refreshTokenValiditySeconds(refreshTokenTTL);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 				.tokenStore(getTokenStore())
+				.accessTokenConverter(getAcessTokenConverter())
+				.reuseRefreshTokens(false)
 				.authenticationManager(authenticationManager);
 	}
 
 	@Bean
-	public TokenStore getTokenStore() {
-		return new InMemoryTokenStore();
+	public JwtAccessTokenConverter getAcessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("algaworks");
+		return jwtAccessTokenConverter;
 	}
 
+	@Bean
+	public TokenStore getTokenStore() {
+		return new JwtTokenStore(getAcessTokenConverter());
+	}
 
+	public int getTokenTTL() {
+		return tokenTTL;
+	}
+
+	public void setTokenTTL(int tokenTTL) {
+		this.tokenTTL = tokenTTL;
+	}
+
+	public int getRefreshTokenTTL() {
+		return refreshTokenTTL;
+	}
+
+	public void setRefreshTokenTTL(int refreshTokenTTL) {
+		this.refreshTokenTTL = refreshTokenTTL;
+	}
 }
---
+++ src/main/java/com/feliperios/algamoneyapi/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,61 @@
+package com.feliperios.algamoneyapi.token;
+
+import com.feliperios.algamoneyapi.config.AuthorizationServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+	@Autowired
+	AuthorizationServerConfig authorizationServerConfig;
+
+	@Override
+	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return returnType.getMethod().getName().equals("postAccessToken");
+	}
+
+	@Override
+	public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
+		HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+		HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+		DefaultOAuth2AccessToken defaultOAuth2AccessToken = (DefaultOAuth2AccessToken) body;
+
+		refreshTokenToCookie(defaultOAuth2AccessToken, servletRequest, servletResponse);
+
+		return body;
+	}
+
+	// Move o Refresh Token do token de acesso do OAuth2 para um cookie.
+	private void refreshTokenToCookie(DefaultOAuth2AccessToken token, HttpServletRequest request, HttpServletResponse response) {
+		// Recupera o campo refresh_token do token de acesso do OAuth2 (JSON)
+		String refreshToken = token.getRefreshToken().getValue();
+		if (refreshToken == null) System.out.println("Houston...");
+		// Cria o cookie e define as propriedades
+		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setSecure(false);
+		refreshTokenCookie.setPath(request.getContextPath() + "/oauth/token");
+		refreshTokenCookie.setMaxAge(authorizationServerConfig.getRefreshTokenTTL());
+		// Adiciona o cookie à resposta
+		response.addCookie(refreshTokenCookie);
+		// Remove o campo refresh_token da estrutura do token de acesso do OAuth2.
+		token.setRefreshToken(null);
+	}
+
+}
